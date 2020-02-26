ad115b840762e6d07dbfffacaa6093b60d1feb30
Christian Trevino <ctrevino1994@yahoo.com>
2020-02-26 03:38:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(current line of people, name) {
   array.push('Welcome, ${name},you are number ${current line of people} in line, ')
-  return 'Welcome, Ada. You are number 1 in line.'
+  
 }
